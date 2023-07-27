5d542cb58fcf84d32ab88d5df05ae7ed263c66e1
julien33510 <120409256+julien33510@users.noreply.github.com>
2023-07-27 18:18:01 +0200

    Structure de fichiers pages/home

--- kasa/src/pages/home/Home.jsx
+++ kasa/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
-import logo from '../assets/logo.svg';
-import '../styles/App.css';
+import logo from '../../assets/logo.svg';
+import '../../styles/index.css';
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
